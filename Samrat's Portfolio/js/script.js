d9178165f2a1bdb2e5b22aaa917b95505ef27f8a
Samrat Mitra <samratmitra338@gmail.com>
2021-10-02 11:37:36 +0530

    Added the Portfolio of Samrat Mitra

---
+++ Samrat's Portfolio/js/script.js
@@ -0,0 +1,16 @@
+const navBtn = document.querySelector('#navbar-toggler');
+const navDiv = document.querySelector('.navbar-collapse');
+
+navBtn.addEventListener('click', () => {
+    navDiv.classList.toggle('showNav');
+});
+
+// stopping animation and transition during window resizing
+let resizeTimer;
+window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+});
