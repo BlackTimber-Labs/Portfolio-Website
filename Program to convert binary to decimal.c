98c734443fc47362fa8872333f1f5938b66a8729
pg-13-007 <66906534+pg-13-007@users.noreply.github.com>
2021-10-03 14:12:30 +0530

    Create Program to convert binary to decimal.c

---
+++ Program to convert binary to decimal.c
@@ -0,0 +1,29 @@
+// convert binary to decimal
+
+#include <stdio.h>
+#include <math.h>
+
+// function prototype
+int convert(long long);
+
+int main() {
+  long long n;
+  printf("Enter a binary number: ");
+  scanf("%lld", &n);
+  printf("%lld in binary = %d in decimal", n, convert(n));
+  return 0;
+}
+
+// function definition
+int convert(long long n) {
+  int dec = 0, i = 0, rem;
+
+  while (n!=0) {
+    rem = n % 10;
+    n /= 10;
+    dec += rem * pow(2, i);
+    ++i;
+  }
+
+  return dec;
+}
