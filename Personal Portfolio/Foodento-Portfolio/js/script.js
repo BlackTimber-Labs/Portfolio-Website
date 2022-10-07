c4a93a97c1278a86545773a3f073753f49325f56
Ravi Kumar <ravi.rks4488@gmail.com>
2022-10-07 21:56:51 +0530

    Foodento_Portfolio

---
+++ Personal Portfolio/Foodento-Portfolio/js/script.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active')
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active')
+}
