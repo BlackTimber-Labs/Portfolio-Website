14246467cabec1a79b01429161975f7733a3923d
Sanjay <102804548+sanjayk0508@users.noreply.github.com>
2022-10-17 18:29:40 +0530

    Add files via upload

---
+++ Personal Portfolio/Sanjay/components/Projects/Projects.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
+
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title>{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <br />
+            <br />
+            <div>
+              <TitleContent>Language</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>View</ExternalLinks>
+              <ExternalLinks href={source}>Code</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
+    </GridContainer>
+  </Section>
+);
+
+export default Projects;
